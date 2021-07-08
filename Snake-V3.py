ca2c0d7282518921ecf36f2edbabbcab8937a6c1
WendyMH <86991582+WendyMH@users.noreply.github.com>
2021-07-07 23:35:30 -0500

    Creacion de la comida

---
+++ Snake-V3.py
@@ -0,0 +1,73 @@
+import turtle
+import time
+import random
+
+posponer = 0.1
+
+#Configuración de Ventana
+wn = turtle.Screen()
+wn.title("Juego de Pong")
+wn.bgcolor("Black")
+wn.setup(width = 600, height = 600)
+wn.tracer(0)
+
+#Cabeza serpiente
+cabeza = turtle.Turtle()
+cabeza.speed(0)
+cabeza.shape("square")
+cabeza.color("white")
+cabeza.penup()
+cabeza.goto(0,0)
+cabeza.direction = "stop"
+
+#Comida
+comida = turtle.Turtle()
+comida.speed(0)
+comida.shape("circle")
+comida.color("red")
+comida.penup()
+comida.goto(0,100)
+
+
+#Funciones
+def arriba():
+    cabeza.direction = "up"
+def abajo():
+    cabeza.direction = "down"
+def izquierda():
+    cabeza.direction = "left"
+def derecha():
+    cabeza.direction = "right"
+
+def mov():
+    if cabeza.direction == "up":
+        y = cabeza.ycor()
+        cabeza.sety(y + 20)
+    if cabeza.direction == "down":
+        y = cabeza.ycor()
+        cabeza.sety(y - 20)
+    if cabeza.direction == "left":
+        x = cabeza.xcor()
+        cabeza.setx(x - 20)
+    if cabeza.direction == "right":
+        x = cabeza.xcor()
+        cabeza.setx(x + 20)
+#Teclado
+wn.listen()
+wn.onkeypress(arriba, "Up")
+wn.onkeypress(abajo, "Down")
+wn.onkeypress(izquierda, "Left")
+wn.onkeypress(derecha, "Right")
+
+while True:
+    wn.update()
+
+    if cabeza.distance(comida) < 20:
+       x = random.randint(-280,280)
+       y = random.randint(-280,280)
+       comida.goto(x,y)
+
+    mov()
+    time.sleep(posponer)
+
+#Creacion de la comida
